8d2376901749e30bc82c079bcf244d48f8ce2754
Tom MacWright <tom@macwright.org>
2014-12-01 18:18:13 -0500

    Switch to 6to5. Works except for node test-browser.

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -1,15 +1,3 @@
 'use strict';
 
-var traceur = require('traceur');
-
-require('traceur-source-maps').install(traceur);
-
-traceur.require.makeDefault(function(filename) {
-    return !(/node_modules/.test(filename));
-}, {
-    arrowFunctions: true,
-    propertyMethods: true,
-    modules: 'commonjs',
-    propertyNameShorthand: true,
-    asyncFunctions: true
-});
+require('6to5/register');
